bc47095a961c30ac26db58d50da6c8757b06649f
Nivadhini <nivadhini.moodley@mail.utoronto.ca>
2023-11-04 21:05:53 -0400

    Updated SymptomCheckerView.java (unfinished)

--- diagnosisApplication/src/view/SymptomCheckerView.java
+++ diagnosisApplication/src/view/SymptomCheckerView.java
@@ -1,13 +1,53 @@
 package view;
 
+import interface_adapter.diagnosis.DiagnosisController;
+import interface_adapter.symptom_checker.SymptomCheckerController;
+import interface_adapter.symptom_checker.SymptomCheckerState;
+import interface_adapter.symptom_checker.SymptomCheckerViewModel;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class SymptomCheckerView extends JPanel {
     public final String viewName = "symptom checker";
 
-    private final JTextField sexInputField = new JTextField(0);
-    private final JTextField ageInputField = new JTextField(0);
+    private final SymptomCheckerViewModel symptomCheckerViewModel;
+    private final SymptomCheckerController symptomCheckerController;
     private final DiagnosisController diagnosisController;
+
+    private final JButton diagnoses;
+
+    public SymptomCheckerView(SymptomCheckerViewModel symptomCheckerViewModel,
+                              SymptomCheckerController symptomCheckerController,
+                              DiagnosisController diagnosisController) {
+        this.symptomCheckerViewModel = symptomCheckerViewModel;
+        this.symptomCheckerController = symptomCheckerController;
+        this.diagnosisController = diagnosisController;
+
+        JLabel title = new JLabel(SymptomCheckerViewModel.TITLE_LABEL);
+
+        JPanel buttons = new JPanel();
+        diagnoses = new JButton(SymptomCheckerViewModel.DIAGNOSES_BUTTON_LABEL);
+        buttons.add(diagnoses);
+
+        diagnoses.addActionListener(
+                new ActionListener() {
+
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        if (e.getSource().equals(diagnoses)) {
+                            SymptomCheckerState currentState = symptomCheckerViewModel.getState();
+
+                            diagnosisController.execute();
+                        }
+                    }
+                }
+        );
+
+        this.add(title);
+        this.add(buttons);
+    }
 }
