313e06bb6434f36ab397da17cd4272e9807fdb43
Nivadhini <nivadhini.moodley@mail.utoronto.ca>
2023-11-07 16:10:00 -0500

    Filled out Symptom.java; might add interface later

--- diagnosisApplication/src/entities/Symptom.java
+++ diagnosisApplication/src/entities/Symptom.java
@@ -6,4 +6,41 @@ package entities;
 
 public class Symptom {
 
+    private final String symptomName;
+    private boolean present;
+
+    /**
+     * Creates a symptom with the specified name and sets present (state) to false.
+     * @param symptomName the name of the symptom
+     */
+    public Symptom(String symptomName) {
+        this.symptomName = symptomName;
+        this.present = false;
+    }
+
+    /**
+     * Returns the name of the symptom.
+     * @return value of name property
+     */
+    public String getSymptomName() {
+        return symptomName;
+    }
+
+    /**
+     * Returns whether this symptom is present/state of the symptom.
+     * @return value of the present property
+     */
+    public boolean isPresent() {
+        return present;
+    }
+
+    /**
+     * Changes the state of the symptom and returns the changed value.
+     * @return new value of the present property
+     */
+    public boolean select() {
+        this.present = !present;
+        return present;
+    }
+
 }
