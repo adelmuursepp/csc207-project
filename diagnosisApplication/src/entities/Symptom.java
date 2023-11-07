cfc671bff9355b089f5a3e70ea8993bbb3d658dd
Nivadhini <nivadhini.moodley@mail.utoronto.ca>
2023-11-07 16:00:52 -0500

    Added Symptom.java to entities

---
+++ diagnosisApplication/src/entities/Symptom.java
@@ -0,0 +1,9 @@
+package entities;
+/**
+ * A representation of a medically relevant symptom. The symptom has a name and state. The state indicates whether
+ * the symptom is present. The default state of a symptom is not present. Symptoms can be selected and deselected.
+ */
+
+public class Symptom {
+
+}
