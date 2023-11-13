e4721007a404d9c361a0883d8097773c964c9f0e
kailimprice <kailimprice@gmail.com>
2023-11-13 09:20:21 -0500

    Fixed abstract execute method in DiagnosisInputBoundary.java and made DiagnosisInputBoundary class an interface

--- diagnosisApplication/src/use_case/diagnosis/DiagnosisInputBoundary.java
+++ diagnosisApplication/src/use_case/diagnosis/DiagnosisInputBoundary.java
@@ -2,8 +2,6 @@ package use_case.diagnosis;
 
 import java.util.List;
 
-public class DiagnosisInputBoundary {
-    public List<String> execute(DiagnosisInputData diagnosisInputData) {
-        return null;
-    }
+public interface DiagnosisInputBoundary {
+    List<String> execute(DiagnosisInputData diagnosisInputData);
 }
