d131f72bf4db9da53f757a587c3647762f0c956b
dshiister <daniel.j.shi@hotmail.com>
2023-11-25 18:33:59 -0500

    Changed names from SC to SymptomChecker

--- diagnosisApplication/src/use_case/symptom_checker/SymptomCheckerInputBoundary.java
+++ diagnosisApplication/src/use_case/symptom_checker/SymptomCheckerInputBoundary.java
@@ -1,4 +1,6 @@
 package use_case.symptom_checker;
 
-public class SymptomCheckerInputBoundary {
+public interface SymptomCheckerInputBoundary {
+
+    void getID(String symptom);
 }
--- diagnosisApplication/src/use_case/symptom_checker/SymptomCheckerInteractor.java
+++ diagnosisApplication/src/use_case/symptom_checker/SymptomCheckerInteractor.java
@@ -1,4 +1,20 @@
 package use_case.symptom_checker;
 
-public class SymptomCheckerInteractor {
+public class SymptomCheckerInteractor implements SymptomCheckerInputBoundary {
+
+    final SymptomCheckerDataAccessInterface symptomCheckerDataAccessObject;
+
+    public SymptomCheckerInteractor(SymptomCheckerDataAccessInterface symptomCheckerDataAccessInterface) {
+        this.symptomCheckerDataAccessObject = symptomCheckerDataAccessInterface;
+    }
+
+    public void toggle(String symptom) {
+
+        symptomCheckerDataAccessObject.toggle(getID(symptom));
+    }
+
+    public void getID(String symptom) {
+        return symptomCheckerDataAccessObject.getID(symptom);
+    }
+
 }
