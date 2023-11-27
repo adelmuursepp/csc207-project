6b9f4cae06dd772ba8a0d7db1c6b96054f420470
Nivadhini <nivadhini.moodley@mail.utoronto.ca>
2023-11-27 12:44:06 -0500

    Fixed DiagnosisOutputData.java

--- diagnosisApplication/src/main/use_case/diagnosis/DiagnosisOutputData.java
+++ diagnosisApplication/src/main/use_case/diagnosis/DiagnosisOutputData.java
@@ -1,18 +1,49 @@
 package main.use_case.diagnosis;
 
 import java.util.HashMap;
-import java.util.List;
 
 public class DiagnosisOutputData {
 
-    private final List<Integer> checkedSymptoms;
+    private final HashMap<Integer, HashMap<java.lang.String, Object>> diagnosis1;
+    private final HashMap<Integer, HashMap<java.lang.String, Object>> diagnosis2;
+    private final HashMap<Integer, HashMap<java.lang.String, Object>> diagnosis3;
 
-    public DiagnosisOutputData(List<Integer> checkedSymptoms) { this.checkedSymptoms = checkedSymptoms; }
+    public DiagnosisOutputData(HashMap<Integer, HashMap<String, Object>> diagnosis1,
+                               HashMap<Integer, HashMap<String, Object>> diagnosis2,
+                               HashMap<Integer, HashMap<String, Object>> diagnosis3) {
+        this.diagnosis1 = diagnosis1;
+        this.diagnosis2 = diagnosis2;
+        this.diagnosis3 = diagnosis3;
+    }
 
-    public List<Integer> getcheckedsypmtoms() {
-        return checkedSymptoms;
+    public DiagnosisOutputData(HashMap<Integer, HashMap<String, Object>> diagnosis1,
+                               HashMap<Integer, HashMap<String, Object>> diagnosis2) {
+        this.diagnosis1 = diagnosis1;
+        this.diagnosis2 = diagnosis2;
+        this.diagnosis3 = null;
+    }
+
+    public DiagnosisOutputData(HashMap<Integer, HashMap<String, Object>> diagnosis1) {
+        this.diagnosis1 = diagnosis1;
+        this.diagnosis2 = null;
+        this.diagnosis3 = null;
+    }
+
+    public DiagnosisOutputData() {
+        this.diagnosis1 = null;
+        this.diagnosis2 = null;
+        this.diagnosis3 = null;
+    }
+
+    public HashMap<Integer, HashMap<String, Object>> getDiagnosis1() {
+        return diagnosis1;
     }
 
     public HashMap<Integer, HashMap<String, Object>> getDiagnosis2() {
+        return diagnosis2;
+    }
+
+    public HashMap<Integer, HashMap<String, Object>> getDiagnosis3() {
+        return diagnosis3;
     }
 }
