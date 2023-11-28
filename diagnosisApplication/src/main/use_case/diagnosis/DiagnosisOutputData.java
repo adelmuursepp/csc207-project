5e027deef34a9e8d60e953a2f0f3667c3a712ba7
Nivadhini <nivadhini.moodley@mail.utoronto.ca>
2023-11-27 22:20:11 -0500

    fixed a bug in DiagnosisOutputData.java (each diagnosis is a single HashMap now)

--- diagnosisApplication/src/main/use_case/diagnosis/DiagnosisOutputData.java
+++ diagnosisApplication/src/main/use_case/diagnosis/DiagnosisOutputData.java
@@ -4,26 +4,26 @@ import java.util.HashMap;
 
 public class DiagnosisOutputData {
 
-    private final HashMap<Integer, HashMap<java.lang.String, Object>> diagnosis1;
-    private final HashMap<Integer, HashMap<java.lang.String, Object>> diagnosis2;
-    private final HashMap<Integer, HashMap<java.lang.String, Object>> diagnosis3;
+    private final HashMap<String, Object> diagnosis1;
+    private final HashMap<String, Object> diagnosis2;
+    private final HashMap<String, Object> diagnosis3;
 
-    public DiagnosisOutputData(HashMap<Integer, HashMap<String, Object>> diagnosis1,
-                               HashMap<Integer, HashMap<String, Object>> diagnosis2,
-                               HashMap<Integer, HashMap<String, Object>> diagnosis3) {
+    public DiagnosisOutputData(HashMap<String, Object> diagnosis1,
+                               HashMap<String, Object> diagnosis2,
+                               HashMap<String, Object> diagnosis3) {
         this.diagnosis1 = diagnosis1;
         this.diagnosis2 = diagnosis2;
         this.diagnosis3 = diagnosis3;
     }
 
-    public DiagnosisOutputData(HashMap<Integer, HashMap<String, Object>> diagnosis1,
-                               HashMap<Integer, HashMap<String, Object>> diagnosis2) {
+    public DiagnosisOutputData(HashMap<String, Object> diagnosis1,
+                               HashMap<String, Object> diagnosis2) {
         this.diagnosis1 = diagnosis1;
         this.diagnosis2 = diagnosis2;
         this.diagnosis3 = null;
     }
 
-    public DiagnosisOutputData(HashMap<Integer, HashMap<String, Object>> diagnosis1) {
+    public DiagnosisOutputData(HashMap<String, Object> diagnosis1) {
         this.diagnosis1 = diagnosis1;
         this.diagnosis2 = null;
         this.diagnosis3 = null;
@@ -35,15 +35,15 @@ public class DiagnosisOutputData {
         this.diagnosis3 = null;
     }
 
-    public HashMap<Integer, HashMap<String, Object>> getDiagnosis1() {
+    public HashMap<String, Object> getDiagnosis1() {
         return diagnosis1;
     }
 
-    public HashMap<Integer, HashMap<String, Object>> getDiagnosis2() {
+    public HashMap<String, Object> getDiagnosis2() {
         return diagnosis2;
     }
 
-    public HashMap<Integer, HashMap<String, Object>> getDiagnosis3() {
+    public HashMap<String, Object> getDiagnosis3() {
         return diagnosis3;
     }
 }
