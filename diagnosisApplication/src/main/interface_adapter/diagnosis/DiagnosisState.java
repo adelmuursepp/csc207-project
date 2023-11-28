ba34408c5ddd6749db5f33766ef036b0cb73f7e2
Nivadhini <nivadhini.moodley@mail.utoronto.ca>
2023-11-27 23:55:32 -0500

    Cleaner version of DiagnosisState.java

--- diagnosisApplication/src/main/interface_adapter/diagnosis/DiagnosisState.java
+++ diagnosisApplication/src/main/interface_adapter/diagnosis/DiagnosisState.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class DiagnosisState {
 
+    private int numDiagnoses = 0;
+
     private HashMap<String, Object> diagnosis1;
     private HashMap<String, Object> diagnosis2;
     private HashMap<String, Object> diagnosis3;
@@ -14,14 +16,17 @@ public class DiagnosisState {
 
     public void setDiagnosis1(HashMap<String, Object> diagnosis1) {
         this.diagnosis1 = diagnosis1;
+        numDiagnoses += 1;
     }
 
     public void setDiagnosis2(HashMap<String, Object> diagnosis2) {
         this.diagnosis2 = diagnosis2;
+        numDiagnoses += 1;
     }
 
     public void setDiagnosis3(HashMap<String, Object> diagnosis3) {
         this.diagnosis3 = diagnosis3;
+        numDiagnoses += 1;
     }
 
     public Object getDiagnosis1(String key) {
