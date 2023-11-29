cc11f2463c0b3f9850916b21a2181f8cfc7e11dc
Nivadhini <144376284+Nivadhini@users.noreply.github.com>
2023-11-29 01:00:44 -0500

    Merging branches

--- diagnosisApplication/src/main/use_case/diagnosis/DiagnosisOutputData.java
+++ diagnosisApplication/src/main/use_case/diagnosis/DiagnosisOutputData.java
@@ -1,18 +1,49 @@
 package main.use_case.diagnosis;
 
 import java.util.HashMap;
-import java.util.List;
 
 public class DiagnosisOutputData {
 
-    private final List<Integer> checkedSymptoms;
+    private final HashMap<String, Object> diagnosis1;
+    private final HashMap<String, Object> diagnosis2;
+    private final HashMap<String, Object> diagnosis3;
 
-    public DiagnosisOutputData(List<Integer> checkedSymptoms) { this.checkedSymptoms = checkedSymptoms; }
-
-    public List<Integer> getcheckedsypmtoms() {
-        return checkedSymptoms;
+    public DiagnosisOutputData(HashMap<String, Object> diagnosis1,
+                               HashMap<String, Object> diagnosis2,
+                               HashMap<String, Object> diagnosis3) {
+        this.diagnosis1 = diagnosis1;
+        this.diagnosis2 = diagnosis2;
+        this.diagnosis3 = diagnosis3;
     }
 
-    public HashMap<Integer, HashMap<String, Object>> getDiagnosis2() {
+    public DiagnosisOutputData(HashMap<String, Object> diagnosis1,
+                               HashMap<String, Object> diagnosis2) {
+        this.diagnosis1 = diagnosis1;
+        this.diagnosis2 = diagnosis2;
+        this.diagnosis3 = null;
+    }
+
+    public DiagnosisOutputData(HashMap<String, Object> diagnosis1) {
+        this.diagnosis1 = diagnosis1;
+        this.diagnosis2 = null;
+        this.diagnosis3 = null;
+    }
+
+    public DiagnosisOutputData() {
+        this.diagnosis1 = null;
+        this.diagnosis2 = null;
+        this.diagnosis3 = null;
+    }
+
+    public HashMap<String, Object> getDiagnosis1() {
+        return diagnosis1;
+    }
+
+    public HashMap<String, Object> getDiagnosis2() {
+        return diagnosis2;
+    }
+
+    public HashMap<String, Object> getDiagnosis3() {
+        return diagnosis3;
     }
 }
