8a747a8debd9a06d4acd4f4599396590c4600c26
Nivadhini <144376284+Nivadhini@users.noreply.github.com>
2023-11-30 18:15:34 -0500

    Created Symptom checker Use Case

--- diagnosisApplication/src/main/use_case/symptom_checker/SymptomCheckerInteractor.java
+++ diagnosisApplication/src/main/use_case/symptom_checker/SymptomCheckerInteractor.java
@@ -4,19 +4,9 @@ import use_case.symptom_checker.SymptomCheckerDataAccessInterface;
 
 public class SymptomCheckerInteractor implements SymptomCheckerInputBoundary {
 
-    final SymptomCheckerDataAccessInterface symptomCheckerDataAccessObject;
+    final SymptomCheckerOutputBoundary symptomCheckerPresenter;
 
-    public SymptomCheckerInteractor(SymptomCheckerDataAccessInterface symptomCheckerDataAccessInterface) {
-        this.symptomCheckerDataAccessObject = symptomCheckerDataAccessInterface;
+    public SymptomCheckerInteractor(SymptomCheckerOutputBoundary symptomCheckerPresenter) {
+        this.symptomCheckerPresenter = symptomCheckerPresenter;
     }
-
-//    public void toggle(String symptom) {
-//
-//        symptomCheckerDataAccessObject.toggle(getID(symptom));
-//    }
-//
-//    public void getID(String symptom) {
-//        return symptomCheckerDataAccessObject.getID(symptom);
-//    }
-
 }
