e162d798dd837d05741c4f05d2f4023c044e16a9
Nivadhini <144376284+Nivadhini@users.noreply.github.com>
2023-11-29 21:33:18 -0500

    Had MedicAPIDiagnosisDataAccessObject.java implement DiagnosisUserDataAccessInterface.java

--- diagnosisApplication/src/main/use_case/diagnosis/DiagnosisUserDataAccessInterface.java
+++ diagnosisApplication/src/main/use_case/diagnosis/DiagnosisUserDataAccessInterface.java
@@ -2,8 +2,9 @@ package main.use_case.diagnosis;
 
 import main.entity.HealthDiagnosis;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface DiagnosisUserDataAccessInterface {
-    List<HealthDiagnosis> getDiagnoses(List<Integer> symptomsList);
+    List<HealthDiagnosis> getDiagnoses(List<Integer> symptomsList) throws IOException, InterruptedException;
 }
