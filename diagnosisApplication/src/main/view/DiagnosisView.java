4d30a479e57bfd40cca93f631fca53a0e024837a
Nivadhini <144376284+Nivadhini@users.noreply.github.com>
2023-12-01 01:21:32 -0500

    View appears; Layout to be altered

--- diagnosisApplication/src/main/view/DiagnosisView.java
+++ diagnosisApplication/src/main/view/DiagnosisView.java
@@ -1,9 +1,7 @@
 package main.view;
 
-import main.interface_adapter.diagnosis.DiagnosisController;
 import main.interface_adapter.diagnosis.DiagnosisState;
 import main.interface_adapter.diagnosis.DiagnosisViewModel;
-import main.interface_adapter.login.LoginState;
 import main.interface_adapter.symptom_checker.SymptomCheckerController;
 
 import javax.swing.*;
@@ -21,7 +19,6 @@ public class DiagnosisView extends JPanel implements ActionListener, PropertyCha
 
     private final DiagnosisViewModel diagnosisViewModel;
     private final SymptomCheckerController symptomCheckerController;
-    private final DiagnosisState diagnosisState;
 
     private final JButton symptomChecker;
 
@@ -29,59 +26,8 @@ public class DiagnosisView extends JPanel implements ActionListener, PropertyCha
         this.diagnosisViewModel = diagnosisViewModel;
         this.symptomCheckerController = symptomCheckerController;
         diagnosisViewModel.addPropertyChangeListener(this);
-        this.diagnosisState = diagnosisViewModel.getState();
-
-        int n = diagnosisState.getNumDiagnoses();
 
         JLabel title = new JLabel(DiagnosisViewModel.TITLE_LABEL);
-
-        JPanel information = new JPanel();
-        for (int i = 0; i < n; i++) {
-            JPanel infoPanel = new JPanel();
-            HashMap<String, Object> currentDiagnosis;
-            if (i == 2) {
-                currentDiagnosis = diagnosisState.getDiagnosis3();
-            } else if (i == 1) {
-                currentDiagnosis = diagnosisState.getDiagnosis2();
-            } else {
-                currentDiagnosis = diagnosisState.getDiagnosis1();
-            }
-            StringBuilder builder = new StringBuilder();
-            builder.append(currentDiagnosis.get("name")).append(" (")
-                    .append(currentDiagnosis.get("accuracy")).append(" likelihood").append(")").append("\n");
-            builder.append("\t\t" + "Professional Name: ").append(currentDiagnosis.get("profName")).append("\n");
-            builder.append("\t\t" + "ICD Number: ").append(currentDiagnosis.get("icd")).append("\n");
-            builder.append("Specialists:" + "\n");
-            for (String specialist : (List<String>) currentDiagnosis.get("specialists")) {
-                builder.append("\t\t").append(specialist).append("\n");
-            }
-            JLabel info = new JLabel(builder.toString());
-            infoPanel.add(info);
-            information.add(infoPanel, BorderLayout.SOUTH);
-        }
-        JLabel infoPanelLabel = new JLabel(DiagnosisViewModel.INFO_PANEL_LABEL);
-        information.add(infoPanelLabel, BorderLayout.NORTH);
-
-        // This is the code to create and add the bar chart on the condition that there is at least one diagnosis.
-        // I will add something similar for the information panel.
-        JPanel barChart = new JPanel();
-        if (n > 0) {
-            int[] accuracies = new int[n];
-            String[] diagnoses = new String[n];
-
-            accuracies[0] = (Integer) diagnosisState.getDiagnosis1().get("accuracy");
-            diagnoses[0] = (String) diagnosisState.getDiagnosis1().get("name");
-            if (n >= 2) {
-                accuracies[1] = (Integer) diagnosisState.getDiagnosis2().get("accuracy");
-                diagnoses[1] = (String) diagnosisState.getDiagnosis2().get("name");
-            }
-            if (n == 3) {
-                accuracies[2] = (Integer) diagnosisState.getDiagnosis3().get("accuracy");
-                diagnoses[2] = (String) diagnosisState.getDiagnosis3().get("name");
-            }
-
-            barChart.add(new BarChart(accuracies, diagnoses, "Diagnoses and Accuracies"));
-        }
 
         JPanel buttons = new JPanel();
         symptomChecker = new JButton(DiagnosisViewModel.SYMPTOM_CHECKER_BUTTON_LABEL);
@@ -100,9 +46,7 @@ public class DiagnosisView extends JPanel implements ActionListener, PropertyCha
 
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-        this.add(title, BorderLayout.NORTH);
-        this.add(information, BorderLayout.EAST);
-        this.add(barChart, BorderLayout.WEST);
+        this.add(title);
         this.add(buttons);
     }
 
@@ -113,18 +57,77 @@ public class DiagnosisView extends JPanel implements ActionListener, PropertyCha
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        DiagnosisState state = (DiagnosisState) evt.getNewValue();
-        if (state.getDiagnosis1() == null) {
-            JOptionPane.showMessageDialog(this, "No diagnoses match symptoms selected");
+        DiagnosisState currentState = (DiagnosisState) evt.getNewValue();
+
+        int n = currentState.getNumDiagnoses();
+        System.out.println(n);
+        if (n == 0) {
+            JOptionPane.showMessageDialog(this, currentState.getNoDiagnosesError());
+        }
+        else {
+            JPanel information = new JPanel();
+            JPanel barChart = new JPanel();
+            for (int i = 0; i < n; i++) {
+                JPanel infoPanel = new JPanel();
+                HashMap<String, Object> currentDiagnosis;
+                if (i == 2) {
+                    currentDiagnosis = currentState.getDiagnosis3();
+                } else if (i == 1) {
+                    currentDiagnosis = currentState.getDiagnosis2();
+                    System.out.println(currentState.getDiagnosis2());
+                } else {
+                    currentDiagnosis = currentState.getDiagnosis1();
+                }
+                StringBuilder builder = new StringBuilder();
+                builder.append(currentDiagnosis.get("Name")).append(" (")
+                        .append(currentDiagnosis.get("Accuracy")).append("% likelihood").append(")").append("\n");
+                builder.append("\t\t" + "Professional Name: ").append(currentDiagnosis.get("ProfName")).append("\n");
+                builder.append("\t\t" + "ICD Number: ").append(currentDiagnosis.get("Icd")).append("\n");
+                builder.append("\t\t" + "ICD Name: ").append(currentDiagnosis.get("IcdName")).append("\n");
+                builder.append("Specialists:" + "\n");
+                System.out.println(builder);
+                for (String specialist : (List<String>) currentDiagnosis.get("Specializations")) {
+                    builder.append("\t\t").append(specialist).append("\n");
+                }
+                System.out.println(builder);
+                JLabel info = new JLabel(builder.toString());
+                infoPanel.add(info);
+                information.add(infoPanel, BorderLayout.SOUTH);
+            }
+            JLabel infoPanelLabel = new JLabel(DiagnosisViewModel.INFO_PANEL_LABEL);
+            information.add(infoPanelLabel);
+            this.add(information);
+
+
+        // This is the code to create and add the bar chart on the condition that there is at least one diagnosis.
+        // I will add something similar for the information panel.
+
+
+            Float[] accuracies = new Float[n];
+            String[] diagnoses = new String[n];
+
+            accuracies[0] = (Float) currentState.getDiagnosis1().get("Accuracy");
+            diagnoses[0] = (String) currentState.getDiagnosis1().get("Name");
+            if (n >= 2) {
+                accuracies[1] = (Float) currentState.getDiagnosis2().get("Accuracy");
+                diagnoses[1] = (String) currentState.getDiagnosis2().get("Name");
+            }
+            if (n == 3) {
+                accuracies[2] = (Float) currentState.getDiagnosis3().get("Accuracy");
+                diagnoses[2] = (String) currentState.getDiagnosis3().get("Name");
+            }
+
+            barChart.add(new BarChart(accuracies, diagnoses, "Diagnoses and Accuracies"));
+           // this.add(barChart);
         }
     }
 
     private class BarChart extends JPanel {
-        private int[] accuracies;
+        private Float[] accuracies;
         private String[] diagnoses;
         private String title;
 
-        public BarChart(int[] accuracies, String[] diagnoses, String title) {
+        public BarChart(Float[] accuracies, String[] diagnoses, String title) {
             this.accuracies = accuracies;
             this.diagnoses = diagnoses;
             this.title = title;
@@ -136,9 +139,9 @@ public class DiagnosisView extends JPanel implements ActionListener, PropertyCha
                 return;
             }
 
-            int min = 0;
-            int max = 0;
-            for (int accuracy : accuracies) {
+            double min = 0;
+            double max = 0;
+            for (Float accuracy : accuracies) {
                 if (min > accuracy) {
                     min = accuracy;
                 }
