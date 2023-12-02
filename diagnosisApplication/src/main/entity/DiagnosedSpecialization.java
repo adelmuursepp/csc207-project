135b7c033138bfc06c56e92efcb41522f975aa9b
Nivadhini <144376284+Nivadhini@users.noreply.github.com>
2023-12-01 22:33:57 -0500

    Merge debug-diagnosis-use-case into obamacare

--- diagnosisApplication/src/main/entity/DiagnosedSpecialization.java
+++ diagnosisApplication/src/main/entity/DiagnosedSpecialization.java
@@ -4,15 +4,10 @@ public class DiagnosedSpecialization {
     private int id;
     private String name;
     private int specialistId;
-    private float accuracy;
-    private String ranking;
-
-    public DiagnosedSpecialization(int id, String name, int specialistId, float accuracy, String ranking) {
-        this.id = id;
-        this.name = name;
-        this.specialistId = specialistId;
-        this.accuracy = accuracy;
-        this.ranking = ranking;
+    public DiagnosedSpecialization(int ID, String Name, int SpecialistID) {
+        this.id = ID;
+        this.name = Name;
+        this.specialistId = SpecialistID;
     }
 
     public String getName() {
